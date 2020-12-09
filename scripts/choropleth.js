327e54c4883a8dea4df01deff6d52a1f03e253af
Kurt Huebner <krh57@cornell.edu>
2020-12-09 16:00:35 -0500

    Implemented mouseover interactivity for main graph

--- scripts/choropleth.js
+++ scripts/choropleth.js
@@ -5,21 +5,22 @@ const margin = { top: 20, right: 20, bottom: 20, left:20};
 const mapWidth = width - margin.left - margin.right;
 const mapHeight = height - margin.top - margin.bottom;
 const map = svg.append("g")
-                .attr("transform","translate("+margin.left+","+margin.top+")");
+                .attr("transform","translate("+margin.left+","+margin.top+")")
+                .attr('pointer-events', 'all');
   
 const loadData = async function() {
     var usa = await d3.json("./data/counties-10m.json");
+    //Broadband data courtesy of Amit Misra, Allen Kim, John Kahan, Juan M. Lavista Ferres at Microsoft Research. See https://github.com/microsoft/USBroadbandUsagePercentages
     var broadband = await d3.csv("./data/broadband_data.csv");
+    //Voter tunrout data calculated using 2016 presidential election data from the MIT Election Lab and US Census population surveys. 
+    //MIT Election Data and Science Lab, 2018, "County Presidential Election Returns 2000-2016", https://doi.org/10.7910/DVN/VOQCHQ, Harvard Dataverse, V6, UNF:6:ZZe1xuZ5H2l4NUiSRcRf8Q== [fileUNF] 
     var voterTurnout = await d3.csv("./data/voterturnout.csv");
-    console.log(usa);
     var counties = topojson.feature(usa, usa.objects.counties);
     var states = topojson.feature(usa, usa.objects.states);
     var statesMesh = topojson.mesh(usa, usa.objects.states);
     var countiesMesh = topojson.mesh(usa, usa.objects.counties);
     projectionUSA = d3.geoAlbersUsa().fitSize([mapWidth, mapHeight], states);
     path = d3.geoPath().projection(projectionUSA);
-            console.log(states);
-            console.log(statesMesh)
 
     var broadbandByID = {};
 
@@ -31,6 +32,12 @@ const loadData = async function() {
 
     voterTurnout.forEach(function(d) {
         turnoutByID[d.ID] = +d.TURNOUT;
+    });
+
+    var nameByID = {};
+
+    voterTurnout.forEach(function(d) {
+      nameByID[d.ID] = d.GEONAME;
     });
 
     let xArray = [];
@@ -65,98 +72,159 @@ const loadData = async function() {
         .attr("class", "county")
         .attr("note", d => d.id)
         .attr("d", path)
-        .style("fill", d => getColor(d.id));
+        .style("fill", d => getColor(d.id))
+        .on('mouseover', mouseover)
+        .on('mouseout', mouseout);    
+      
+
+    map.append('rect')
+        .attr('x', 825)
+        .attr('y', 380)
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('stroke', 'black')
+        .attr('fill', '#e8e8e8');
+              
+    map.append('rect')
+        .attr('x', 850)
+        .attr('y', 380)
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('stroke', 'black')
+        .attr('fill', '#ace4e4');
+                          
+    map.append('rect')
+        .attr('x', 875)
+        .attr('y', 380)
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('stroke', 'black')
+        .attr('fill', '#5ac8c8');
+              
+    map.append('rect')
+        .attr('x', 825)
+        .attr('y', 405)
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('stroke', 'black')
+        .attr('fill', '#dfb0d6');
+              
+    map.append('rect')
+        .attr('x', 850)
+        .attr('y', 405)
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('stroke', 'black')
+        .attr('fill', '#a5add3');
+              
+    map.append('rect')
+        .attr('x', 875)
+        .attr('y', 405)
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('stroke', 'black')
+        .attr('fill', '#5698b9');
+              
+    map.append('rect')
+        .attr('x', 825)
+        .attr('y', 430)
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('stroke', 'black')
+        .attr('fill', '#be64ac');
+                             
+    map.append('rect')
+        .attr('x', 850)
+        .attr('y', 430)
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('stroke', 'black')
+        .attr('fill', '#8c62aa');
+              
+    map.append('rect')
+        .attr('x', 875)
+        .attr('y', 430)
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('stroke', 'black')
+        .attr('fill', '#3b4994');
+    
+    //Legend text
+    map.append('text')
+        .attr("transform", "translate(825,373)")
+        .text("Lower Voter Turnout >")
+        .style("font-size", "9px"); 
+
+    map.append('text')
+         .attr("transform", "translate(825,373)")
+         .text("Lower Voter Turnout >")
+         .style("font-size", "9px");
+
+    map.append('text')
+         .attr("transform", "translate(818,460) rotate (270)")
+         .text("< Lower Broadband")
+         .style("text-anchor", "start")
+         .style("font-size", "10px");
+
+    //Mouseover text
+
+    map.append("text")
+       .text("")
+       .attr("x", 525)
+       .attr("y", 500)
+       .attr("id", "legend_name")
+       .style("font-size", "10pt");
+
+    map.append("text")
+         .text("")
+         .attr("x", 525)
+         .attr("y", 525)
+         .attr("id", "legend_top")
+         .style("font-size", "10pt");
+
+
+    map.append("text")
+         .text("")
+         .attr("x", 525)
+         .attr("y", 550)
+         .attr("id", "legend_bot")
+         .style("font-size", "10pt");
+
+ 
+    function mouseover(d, i) {
+      map.select("#legend_name")
+         .text(nameByID[d.id]);
+
+       map.select("#legend_top")
+          .text("Broaband Penetration: " + broadbandByID[d.id] + "%");
+
+       map.select("#legend_bot")
+          .text("Votor Turnout: " + turnoutByID[d.id] + "%");
+      
+      map.selectAll(".county")
+         .attr("opacity", 0.6);
+
+      d3.select(this)
+         .attr("opacity", 1)
+         .style("stroke-width", "2px");
+    }
+ 
+    function mouseout(d, i) {
+      map.select("#legend_name")
+         .text("");
+
+      map.select("#legend_top")
+          .text("");
+
+      map.select("#legend_bot")
+         .text("");
             
-            map.append("path").datum(statesMesh)
-                .attr("class","outline")
-                .attr("d", path)
-                .style("stroke-width", "2px"); 
+      map.selectAll(".county")
+         .attr("opacity", 1);
 
-            map.append("path").datum(countiesMesh)
-               .attr("class","outline")
-               .attr("d", path); 
-
-               map.append('rect')
-                  .attr('x', 825)
-                  .attr('y', 380)
-                  .attr('width', 25)
-                  .attr('height', 25)
-                  .attr('stroke', 'black')
-                  .attr('fill', '#e8e8e8');
-                        
-                map.append('rect')
-                  .attr('x', 850)
-                  .attr('y', 380)
-                  .attr('width', 25)
-                  .attr('height', 25)
-                  .attr('stroke', 'black')
-                  .attr('fill', '#ace4e4');
-                                    
-                map.append('rect')
-                  .attr('x', 875)
-                  .attr('y', 380)
-                  .attr('width', 25)
-                  .attr('height', 25)
-                  .attr('stroke', 'black')
-                  .attr('fill', '#5ac8c8');
-                        
-                map.append('rect')
-                  .attr('x', 825)
-                  .attr('y', 405)
-                  .attr('width', 25)
-                  .attr('height', 25)
-                  .attr('stroke', 'black')
-                  .attr('fill', '#dfb0d6');
-                        
-                map.append('rect')
-                  .attr('x', 850)
-                  .attr('y', 405)
-                  .attr('width', 25)
-                  .attr('height', 25)
-                  .attr('stroke', 'black')
-                  .attr('fill', '#a5add3');
-                        
-                map.append('rect')
-                  .attr('x', 875)
-                  .attr('y', 405)
-                  .attr('width', 25)
-                  .attr('height', 25)
-                  .attr('stroke', 'black')
-                  .attr('fill', '#5698b9');
-                        
-                map.append('rect')
-                  .attr('x', 825)
-                  .attr('y', 430)
-                  .attr('width', 25)
-                  .attr('height', 25)
-                  .attr('stroke', 'black')
-                  .attr('fill', '#be64ac');
-                                       
-                map.append('rect')
-                  .attr('x', 850)
-                  .attr('y', 430)
-                  .attr('width', 25)
-                  .attr('height', 25)
-                  .attr('stroke', 'black')
-                  .attr('fill', '#8c62aa');
-                        
-                map.append('rect')
-                  .attr('x', 875)
-                  .attr('y', 430)
-                  .attr('width', 25)
-                  .attr('height', 25)
-                  .attr('stroke', 'black')
-                  .attr('fill', '#3b4994');
-                        };
-                
-                map.append('text')
-                   .attr("transform", "translate(825,373)")
-                   .text("Lower Voter Turnout >")
-                   .style("font-size", "9px")
-
-                map.append('text')
-                   .attr("transform", "translate(818,460) rotate (270)")
-                   .text("< Lower Broadband")
-                   .style("text-anchor", "start")
-                   .style("font-size", "10px")
-        loadData();
+      d3.select(this)
+        .style("stroke-width", "1px");
+    }
+  };
+  loadData();
+        
